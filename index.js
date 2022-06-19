7909351b49b2c1fee2a60981d3271d5667dfdde6
Brian Munsey <brianmunsey2018@gmail.com>
2022-06-19 19:47:46 -0400

    first trial rendering react elements to the DOM

---
+++ index.js
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+        <ul className="main-content" ><li>Here is one list item</li></ul>
+    )
+}
+
+ReactDOM.render(
+    <div>
+<MainContent />
+</div>
+, document.querySelector("#root"))
