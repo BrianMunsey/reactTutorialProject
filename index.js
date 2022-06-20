dd76f6194ab06a00eccafd971faa775ff00af633
Brian Munsey <brianmunsey2018@gmail.com>
2022-06-19 20:38:03 -0400

    made multiple ways to get elements to the page using react

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function MainContent() {
-    return (
-        <ul className="main-content" ><li>Here is one list item</li></ul>
-    )
-}
+// function MainContent() {
+//     return (
+//         <ul className="main-content" ><li>Here is one list item</li></ul>
+//     )
+// }
+
+// multiple ways to get elements to the dom
+
+const navbar = (
+    <nav>
+        <h1>Here is my header!!</h1>
+        <ul>
+            <li>Pricing!</li>
+            <li>About!</li>
+            <li>Contact!</li>
+        </ul>
+    </nav>
+)
 
 ReactDOM.render(
-    <div>
-<MainContent />
-</div>
+    navbar
 , document.querySelector("#root"))
+
+// ReactDOM.render(
+//     <div>
+// <MainContent />
+// </div>
+// , document.querySelector("#root"))
